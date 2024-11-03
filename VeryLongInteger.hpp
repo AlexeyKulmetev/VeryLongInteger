485d1d19cc250200bfbb92cdb5cea793297eff92
Aleksey Kulmetev <pervoclassnic@gmail.com>
2024-11-03 19:49:11 +0300

    VeryLongInteger class started

---
+++ VeryLongInteger.hpp
@@ -0,0 +1,16 @@
+#ifndef VERY_LONG_INTEGER
+#define VERY_LONG_INTEGER
+
+#include <vector>
+
+class VeryLongInteger {
+private:
+	std::vector<int> digits;
+	bool negative = false;
+
+public:
+
+};
+
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include "VeryLongInteger.hpp"
+
+int main() {
+
+
+	return 0;
+}
